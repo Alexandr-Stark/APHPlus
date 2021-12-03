d43c3671f249b9e949211ddc7effb815fa95f225
Pavlo Shmygelskyi <pavlo.shmygelskyi@gmail.com>
2021-12-03 16:34:30 +0200

    Create start model for login page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Login from './components/login/Login';
+import "./app.scss";
+//import Register from './components/register/Register';
+
+function App() {
+  return <Login />;
+}
+
+export default App;
